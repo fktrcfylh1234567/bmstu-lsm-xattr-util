7b8212e3cae4b3dba0ee9cac710c3e53846200a3
fktrcfylh1234567 <fktrcfylh1234567@yandex.ru>
2019-11-27 18:25:32 +0300

    add remoce command

--- xattr_util.c
+++ xattr_util.c
@@ -94,7 +94,67 @@ void add_gid(char *path, int gid) {
   }
 }
 
-void read_xattr(char *path) {
+void rm_gid(char *path, int gid) {
+  char *attr;
+  int *data;
+  int ret = EXIT_FAILURE;
+  ssize_t size;
+
+  size = getxattr(path, SEC_LABEL, NULL, 0);
+  if (size < 0) {
+    set_gid(path, gid);
+    return;
+  }
+
+  attr = malloc(size);
+  if (attr == NULL) {
+    printf("No memory.\n");
+    return;
+  }
+
+  ret = getxattr(path, SEC_LABEL, attr, size);
+  if (ret < 0) {
+    perror("[-] getxattr failed");
+    return;
+  }
+
+  data = malloc(size - 1);
+  if (data == NULL) {
+    printf("No memory.\n");
+    return;
+  }
+
+  memcpy(data, attr, size - 1);
+
+  int i = 0;
+  int count = (size - 1) / sizeof(int);
+
+  for (; i < count; i++) {
+    if (data[i] == gid) {
+      break;
+    }
+  }
+
+  if (i == count) {
+    printf("No such id.\n");
+    return;
+  }
+
+  for (; i < count - 1; i++) {
+    data[i] = data[i + 1];
+  }
+
+  memcpy(attr, data, size - 1 - sizeof(int));
+  attr[size - sizeof(int)] = '\0';
+
+  ret = setxattr(path, SEC_LABEL, attr, size - sizeof(int), 0);
+
+  if (ret < 0) {
+    perror("[-] setxattr failed");
+  }
+}
+
+void print_xattr(char *path) {
   char *attr;
   int *data;
   int ret = EXIT_FAILURE;
@@ -137,15 +197,16 @@ int main(int argc, char **argv) {
   if (argc < 3) {
     printf("Invalid arguments specified.\n");
     printf("Usage:\n");
-    printf("xattr_util read <file>\n");
+    printf("xattr_util print <file>\n");
     printf("xattr_util add <gid> <file>\n");
     printf("xattr_util remove <gid> <file>\n");
     printf("xattr_util clear <file>\n");
     return 0;
   }
 
-  if (strcmp(argv[1], "read") == 0) {
-    read_xattr(argv[2]);
+  if (strcmp(argv[1], "print") == 0) {
+    print_xattr(argv[2]);
+    return 0;
   }
 
   if (strcmp(argv[1], "add") == 0) {
@@ -155,10 +216,22 @@ int main(int argc, char **argv) {
     }
 
     add_gid(argv[3], atoi(argv[2]));
+    return 0;
+  }
+
+  if (strcmp(argv[1], "remove") == 0) {
+    if (argv[3] == NULL) {
+      printf("Usage: xattr_util remove <gid> <file>\n");
+      return 0;
+    }
+
+    rm_gid(argv[3], atoi(argv[2]));
+    return 0;
   }
 
   if (strcmp(argv[1], "clear") == 0) {
     clear_xattr(argv[2]);
+    return 0;
   }
 
   return 0;
